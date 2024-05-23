4506594b22e0bea92c15d9d9024b0065eb0b834f
Hitesh Gorana <hitesh@noteefy.app>
2024-05-23 16:37:20 +0530

    add pipline to add note

--- apps/access-key/src/access-key.module.ts
+++ apps/access-key/src/access-key.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AccessKeyService } from './services/access-key.service';
-import { AccessKeyAdminController } from './controllers/access-key-admin.controller';
-import { AccessKeyUserController } from './controllers/access-key-user.controller';
-import { RedisModule } from '@nestjs-modules/ioredis';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AccessKey, AccessKeySchema } from './schemas/access-key.schema';
-import { LoggingService } from './services/logging.service';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AccessKeyService } from "./services/access-key.service";
+import { AccessKeyAdminController } from "./controllers/access-key-admin.controller";
+import { AccessKeyUserController } from "./controllers/access-key-user.controller";
+import { RedisModule } from "@nestjs-modules/ioredis";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AccessKey, AccessKeySchema } from "./schemas/access-key.schema";
+import { LoggingService } from "./services/logging.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: ".env",
     }),
     MongooseModule.forFeature([
       { name: AccessKey.name, schema: AccessKeySchema },
@@ -20,15 +20,15 @@ import { LoggingService } from './services/logging.service';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
+        uri: configService.get<string>("MONGODB_URI"),
       }),
       inject: [ConfigService],
     }),
     RedisModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'single',
-        url: configService.get<string>('REDIS_URL'),
+        type: "single",
+        url: configService.get<string>("REDIS_URL"),
       }),
       inject: [ConfigService],
     }),
--- apps/access-key/src/main.ts
+++ apps/access-key/src/main.ts
@@ -1,19 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { AccessKeyModule } from './access-key.module';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { AccessKeyModule } from "./access-key.module";
+import * as dotenv from "dotenv";
+import * as path from "path";
 
 async function bootstrap() {
   // Create the NestJS application
-  dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+  dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
   const app = await NestFactory.create(AccessKeyModule);
 
   // Get the ConfigService
   const configService = app.get(ConfigService);
 
   // Get the port from the environment variables
-  const port = configService.get<number>('ACCESS_KEY_SERVICE_PORT') || 3000;
+  const port = configService.get<number>("ACCESS_KEY_SERVICE_PORT") || 3000;
 
   // Start listening on the specified port
   await app.listen(port);

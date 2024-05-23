b1edfa0c790216f56492dc8c85ccff8d50df668c
Hitesh Gorana <hitesh@noteefy.app>
2024-05-23 16:59:17 +0530

    add readme

--- apps/access-key/src/services/access-key.service.ts
+++ apps/access-key/src/services/access-key.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AccessKey } from '../schemas/access-key.schema';
